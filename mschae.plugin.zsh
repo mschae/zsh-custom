25e8467308ee2a3aafc1fc210d59ea7babdda3a1
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2015-06-03 13:20:02 +0200

    Improvements

--- mschae.plugin.zsh
+++ mschae.plugin.zsh
@@ -13,20 +13,11 @@ compdef _h h
 export EDITOR=vim
 
 alias zshconfig="$EDITOR ~/.zshrc"
-alias omzconfig="$EDITOR ~/.oh-my-zsh"
 alias vimconfig="$EDITOR ~/.vim"
 
 cf() { cat $1 | pbcopy}
 pf() { pbpaste > $1 }
 
-alio $ZDs fs='bundle install && bundle exec foreman start'
-
-alias b=bundle
-alias be='bundle exec'
-
 gcoid() { git branch | grep $1 | xargs git checkout }
 
-unsetopt correct_all
-
-ZSH_THEME="mschae_agnoster"
 DEFAULT_USER="ms2"
